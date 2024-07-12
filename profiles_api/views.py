2f9c4e9cc959ae1e30ca4957c2419ce6c9659a39
hung <hung.pham@defide-ix.com>
2024-07-12 10:39:21 +0700

    add helloApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+  """Test API VIEW"""
+  def get(self, request, format=None):
+    an_apiview = [
+      'Use HTTP mnethods as function',
+      'Is similar asdasdasdas',
+      'hello babe',
+      'you are handsome'
+    ]
+    return Response({'message': 'Hello', 'an_apiview': an_apiview})
